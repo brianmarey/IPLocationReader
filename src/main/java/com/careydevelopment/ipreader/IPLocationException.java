fba9933348676f3c1765221fe6bc71f90eed738d
brianmcarey <careyb@mindspring.com>
2016-04-22 06:55:40 -0400

    finalizing

---
+++ src/main/java/com/careydevelopment/ipreader/IPLocationException.java
@@ -0,0 +1,31 @@
+package com.careydevelopment.ipreader;
+
+public class IPLocationException extends Exception {
+
+	private static final long serialVersionUID = 1442221303228800846L;
+
+	public IPLocationException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public IPLocationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IPLocationException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IPLocationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IPLocationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/com/careydevelopment/ipreader/IPLocationReader.java
@@ -0,0 +1,57 @@
+package com.careydevelopment.ipreader;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.careydevelopment.jsonparser.JSONException;
+import com.careydevelopment.jsonparser.JSONObject;
+import com.careydevelopment.jsonparser.JSONParser;
+
+public class IPLocationReader {
+	private static final Logger LOGGER = LoggerFactory.getLogger(IPLocationReader.class);
+	
+	private static final String JSON_URL = "http://ipinfo.io/json";
+	
+	public static IPLocation getIPLocation() throws IPLocationException {
+		IPLocation ipLocation = null;
+		
+		try {
+			JSONObject json = JSONParser.readJsonFromUrl(JSON_URL);
+			ipLocation = getIPLocationFromJson(json);
+		} catch (JSONException je) {
+			LOGGER.error("Problem parsing JSON!",je);
+			throw new IPLocationException(je);
+		} 		
+		
+		return ipLocation;
+	}
+
+	
+	private static IPLocation getIPLocationFromJson(JSONObject json) throws JSONException{
+		IPLocation ipLocation = new IPLocation();
+		
+		ipLocation.setCity(json.getString("city"));
+		ipLocation.setCountry(json.getString("country"));
+		ipLocation.setHostname(json.getString("hostname"));
+		ipLocation.setIp(json.getString("ip"));
+		ipLocation.setLoc(json.getString("loc"));
+		ipLocation.setOrg(json.getString("org"));
+		ipLocation.setPostal(json.getString("postal"));
+		ipLocation.setRegion(json.getString("region"));
+		
+		return ipLocation;
+	}
+	
+	
+	public static void main(String[] args) {
+		try {
+			IPLocation ipl = getIPLocation();
+			LOGGER.info("IP is " + ipl.getIp());
+		} catch (IPLocationException ip) {
+			ip.printStackTrace();
+		}
+	}
+
+}
